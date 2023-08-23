9b8c0204435c1641dda62c3c63f9dc9d5ae878c7
Amer <amer.buzo.20@size.ba>
2023-08-23 12:55:07 +0200

    SignUpForm extraction

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import Head from 'next/head';
-import {signUp, login} from '@/services/users';
-import { useForm, } from 'react-hook-form';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'; // Import corrected next/router
 import Link from 'next/link';
-import {hash} from 'bcrypt-ts'
+import SignUpForm from '@/components/ui/signup';
+
 
 export interface RegisterFormData {
   name: string;
@@ -14,31 +13,6 @@ export interface RegisterFormData {
 }
 
 export default function Register() {
-  const router = useRouter();
-  const [message, setMessage] = useState<string | null>(''); // Initialize the message state
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterFormData>();
-
-  const onSubmit = async (data: RegisterFormData) => {
-    try {
-      const saltRounds = 10;
-      const hashedPassword = await hash(data.password, saltRounds);
-      data.password = hashedPassword;
-
-    const user = await signUp(data);
-
-      if(user.status === 200){
-        router.push('/login'); // Redirect to login page after successful registration
-      }
-
-    } catch (error) {
-      setMessage('An error occurred during registration.'); // Set a generic error message
-      console.log(error)
-    }
-  };
 
   return (
     <>
@@ -52,49 +26,7 @@ export default function Register() {
         <div className="w-full max-w-md py-12">
           <div className="bg-white p-5 rounded-lg shadow-lg shadow-gray-800">
             <h2 className="text-2xl font-bold mb-6 text-center">Sign in form</h2>
-
-            <form method="post" onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-6">
-                <label className="block mb-2 text-gray-800" htmlFor="name">Name</label>
-                <input
-                  className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
-                  {...register('name')}
-                  placeholder="Enter your name"
-                  type="text"/>
-                <p className="text-red-500"></p>
-              </div>
-
-              <div className="mb-6">
-                <label className="block mb-2 text-gray-800" htmlFor="email">Email</label>
-                <input
-                  className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
-                  {...register('email')}
-                  placeholder="Enter your email"
-                  type="email"/>
-                <p className="text-red-500"></p>
-              </div>
-
-              <div className="mb-6">
-                <label className="block mb-2 text-gray-800" htmlFor="password">Password</label>
-                <input
-                    className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
-                    {...register('password')}
-                    placeholder="Enter your password"
-                    type="password"/>
-                <p className="text-red-500"></p>
-              </div>
-
-              <div className="flex items-center justify mb-6">
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white text-center px-2 py-2 rounded hover:bg-blue-600">Submit</button>
-                <span className="text-dark text-sm text-center px-1">
-                  Already registered?{' '}
-                  <Link className="text-blue-500 hover:text-blue-800 underline" href="/login">Log in here!</Link>
-                </span>
-              </div>
-            </form>
-            {message && <div className="text-red-500">{message}</div>}
+            <SignUpForm/>
           </div>
         </div>
       </div>
--- src/components/ui/signup.tsx
+++ src/components/ui/signup.tsx
@@ -1,7 +1,77 @@
+"use client"
 import React from 'react'
+import {hash} from 'bcrypt-ts'
+import {signUp} from '@/services/users';
+import { useForm, } from 'react-hook-form';
+import { RegisterFormData } from '@/app/register/page';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function SignUpForm() {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<RegisterFormData>();
+    
+      const onSubmit = async (data: RegisterFormData) => {
+        try {
+          const saltRounds = 10;
+          const hashedPassword = await hash(data.password, saltRounds);
+          data.password = hashedPassword;
+    
+        const user = await signUp(data);
+    
+          if(user.status === 200){
+            router.push('/login'); // Redirect to login page after successful registration
+          }
+    
+        } catch (error) {
+          console.log(error)
+        }
+      };
   return (
-    <div>signup</div>
+    <form method="post" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-6">
+                <label className="block mb-2 text-gray-800" htmlFor="name">Name</label>
+                <input
+                  className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
+                  {...register('name')}
+                  placeholder="Enter your name"
+                  type="text"/>
+                <p className="text-red-500"></p>
+              </div>
+
+              <div className="mb-6">
+                <label className="block mb-2 text-gray-800" htmlFor="email">Email</label>
+                <input
+                  className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
+                  {...register('email')}
+                  placeholder="Enter your email"
+                  type="email"/>
+                <p className="text-red-500"></p>
+              </div>
+
+              <div className="mb-6">
+                <label className="block mb-2 text-gray-800" htmlFor="password">Password</label>
+                <input
+                    className="w-full px-4 py-2 border border-gray-300 text-black rounded focus:outline-none focus:border-blue-500"
+                    {...register('password')}
+                    placeholder="Enter your password"
+                    type="password"/>
+                <p className="text-red-500"></p>
+              </div>
+
+              <div className="flex items-center justify mb-6">
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white text-center px-2 py-2 rounded hover:bg-blue-600">Submit</button>
+                <span className="text-dark text-sm text-center px-1">
+                  Already registered?{' '}
+                  <Link className="text-blue-500 hover:text-blue-800 underline" href="/login">Log in here!</Link>
+                </span>
+              </div>
+            </form>
   )
 }
