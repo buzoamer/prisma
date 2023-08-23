7fe835afa02649eeac86c0d69fe8cd63b5556580
Amer <amer.buzo.20@size.ba>
2023-08-23 12:44:02 +0200

    LoginForm extraction

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,8 @@
 
 import React from 'react';
 import Head from 'next/head';
-import { useForm } from 'react-hook-form';
 import { useState } from 'react';
-import Link from 'next/link';
-import { login } from '@/services/users';
-import { useRouter } from 'next/navigation';
+import LoginForm from '@/components/ui/login';
 
 export interface LoginFormData {
   email: string;
@@ -20,33 +17,7 @@ interface ApiResponse {
 
 
 const Login = () => {
-  const router = useRouter();
   const [message, setMessage] = useState<string | null>('');
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<LoginFormData>();
-  const onSubmit = async (data: LoginFormData) => {
-    const res= await login(data);
-
-    if(res.status!=200){
-      console.log(res)
-      return setMessage(res.message)
-
-    } 
-    router.push("/register")
-
-  /* const userRes = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(data)
-    })
-    console.log(userRes) */
-  };
-
   return (
     <>
       <Head>
@@ -60,39 +31,7 @@ const Login = () => {
           <div className="max-w-lg py-1 mx-auto">
             <div className="bg-purple-600 bg-opacity-50 p-16 m-2 rounded-lg shadow-lg shadow-pink-800">
               <h2 className="text-center text-white text-2xl font-bold mb-4">Log in form</h2>
-
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                  <label htmlFor="email" className="block mb-2 text-white">Email</label>
-                  <input
-                    {...register('email')}
-                    type="text"
-                    id="email"
-                    name="email"
-                    className="w-full px-4 py-2 bg-pink-500 text-white bg-opacity-70 border border-gray-300 rounded focus:outline-none focus:bg-pink-900 focus:border-white placeholder:text-slate-300"
-                    placeholder="Enter your email"/>
-                </div>
-
-                <div className="mb-4">
-                  <label htmlFor="password" className="block text-white">Password</label>
-                  <input
-                    {...register('password')}
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full px-4 py-2 text-white bg-pink-500 bg-opacity-70 border border-gray-300 rounded focus:outline-none focus:bg-pink-900 focus:border-white placeholder:text-slate-300"
-                    placeholder="Enter your password"/>
-                </div>
-
-                <div className="flex items-center">
-                  <button
-                    className="bg-pink-600 text-white text-center px-2 py-2 rounded hover:bg-pink-900">Submit</button>
-                  <span className="text-white text-md px-4 text-center">
-                    Not registered?
-                    <Link className="text-white underline hover:text-black px-2 font-semibold" href="/register">Do it here!</Link>
-                  </span>
-                </div>
-              </form>
+              <LoginForm/>
               {message && <div className="mt-2 text-black font-semibold">{message}</div>}
             </div>
           </div>
---
+++ src/components/ui/login.tsx
@@ -0,0 +1,65 @@
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { login } from '@/services/users';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { LoginFormData } from '@/app/login/page';
+import Link from 'next/link';
+
+export default function LoginForm() {
+    const router = useRouter();
+  const [message, setMessage] = useState<string | null>('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginFormData>();
+  const onSubmit = async (data: LoginFormData) => {
+    const res= await login(data);
+
+    if(res.status!=200){
+      console.log(data)
+      setMessage(res.message)
+      console.log("Invalid credentials")
+    } else {
+        router.push('/events')
+        
+    }
+    
+
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4">
+                  <label htmlFor="email" className="block mb-2 text-white">Email</label>
+                  <input
+                    {...register('email')}
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full px-4 py-2 bg-pink-500 text-white bg-opacity-70 border border-gray-300 rounded focus:outline-none focus:bg-pink-900 focus:border-white placeholder:text-slate-300"
+                    placeholder="Enter your email"/>
+                </div>
+
+                <div className="mb-4">
+                  <label htmlFor="password" className="block text-white">Password</label>
+                  <input
+                    {...register('password')}
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="w-full px-4 py-2 text-white bg-pink-500 bg-opacity-70 border border-gray-300 rounded focus:outline-none focus:bg-pink-900 focus:border-white placeholder:text-slate-300"
+                    placeholder="Enter your password"/>
+                </div>
+
+                <div className="flex items-center">
+                  <button
+                    className="bg-pink-600 text-white text-center px-2 py-2 rounded hover:bg-pink-900">Submit</button>
+                  <span className="text-white text-md px-4 text-center">
+                    Not registered?
+                    <Link className="text-white underline hover:text-black px-2 font-semibold" href="/register">Do it here!</Link>
+                  </span>
+                </div>
+              </form>
+  )
+}
